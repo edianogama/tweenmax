9cb9f72865b585a2926712ca20cf93268ba3c76e
Ediano <eddy.tadim@gmail.com>
2019-01-08 16:21:09 -0200

    slideshow simples

--- slide/js/app.js
+++ slide/js/app.js
@@ -1,13 +1,51 @@
-const tl = new TimelineMax();
-
-
 const sliderItems = document.querySelectorAll('.slider__item');
-const texts = sliderItems[0].querySelectorAll('p');
+const btnNext = document.querySelector('.slider__arrows--right');
+const btnPrev = document.querySelector('.slider__arrows--left');
 
-TweenMax.set(sliderItems[0], {scale: .9});
+const SlideShow = {
+    currentItem: 0,
+    
+    init: () => {
+        SlideShow.in(SlideShow.currentItem);
+    }, 
+    in: (index) =>  {
+        const item = sliderItems[index];
+        const texts = item.querySelectorAll('p');
+      
+        TweenMax.set(item, {scale: .9});
+        TweenMax.set(item, {left: '-100vw'});
 
-tl
-.to( sliderItems[0], .5, { left: 0, delay: 1} )
-.to( sliderItems[0], .3, {scale: 1,  ease: Power0.easeInOut } )
-.staggerFrom(texts, .4, { y: 300, autoAlpha: 0, ease: Back.easeInOut }, .4 );
+        const tl = new TimelineMax();
+        tl
+        .to( item, .5, { left: 0, delay: 1} )
+        .to( item, .3, {scale: 1,  ease: Power0.easeInOut } )
+        .staggerFrom(texts, .4, { y: 300, autoAlpha: 0, ease: Back.easeOut }, .4 );
+    },
+    out: (index, nextItem) => {
+        const item = sliderItems[index];
+        const texts = item.querySelectorAll('p');
+        
+        const tl = new TimelineMax();
+        tl
+        .staggerTo(texts, .4, { y: 300, autoAlpha: 0, ease: Back.easeIn }, '-.5')
+        .to( item, .3, {scale: .9,  ease: Power0.easeInOut } )
+        .to( item, .5, { left: '100vw'} )
+        .call(SlideShow.in, [nextItem], this, '-=1.5')
+        .set(texts, { clearProps: 'all' });
+    },
+    next: () => {
+        const next = SlideShow.currentItem !== sliderItems.length -1 ? SlideShow.currentItem +1 : 0;
+        SlideShow.out(SlideShow.currentItem, next);
+        SlideShow.currentItem = next;
+    },
+    prev: () => {
+        const prev = SlideShow.currentItem > 0 ? SlideShow.currentItem -1 : sliderItems.length -1;
+        SlideShow.out(SlideShow.currentItem, prev);
+        SlideShow.currentItem = prev;
 
+    }
+}
+
+SlideShow.init();
+btnNext.addEventListener('click', SlideShow.next);
+btnPrev.addEventListener('click', SlideShow.prev);
